c6aa31e4c64ab8626116935b668f6b119c3bd4f0
Zubayer Rahaman <zubayer.skd@gmail.com>
2023-03-07 19:09:11 +0600

    fixed bug

--- message_receiver.py
+++ message_receiver.py
@@ -49,74 +49,74 @@ def send_mail(email, slot_date):
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
 	<style>
-		* {
+		* {{
 			box-sizing: border-box;
-		}
+		}}
 
-		body {
+		body {{
 			margin: 0;
 			padding: 0;
-		}
+		}}
 
-		a[x-apple-data-detectors] {
+		a[x-apple-data-detectors] {{
 			color: inherit !important;
 			text-decoration: inherit !important;
-		}
+		}}
 
-		#MessageViewBody a {
+		#MessageViewBody a {{
 			color: inherit;
 			text-decoration: none;
-		}
+		}}
 
-		p {
+		p {{
 			line-height: inherit
-		}
+		}}
 
 		.desktop_hide,
-		.desktop_hide table {
+		.desktop_hide table {{
 			mso-hide: all;
 			display: none;
 			max-height: 0px;
 			overflow: hidden;
-		}
+		}}
 
-		.image_block img+div {
+		.image_block img+div {{
 			display: none;
-		}
+		}}
 
-		@media (max-width:720px) {
-			.social_block.desktop_hide .social-table {
+		@media (max-width:720px) {{
+			.social_block.desktop_hide .social-table {{
 				display: inline-block !important;
-			}
+			}}
 
 			.image_block img.big,
-			.row-content {
+			.row-content {{
 				width: 100% !important;
-			}
+			}}
 
-			.mobile_hide {
+			.mobile_hide {{
 				display: none;
-			}
+			}}
 
-			.stack .column {
+			.stack .column {{
 				width: 100%;
 				display: block;
-			}
+			}}
 
-			.mobile_hide {
+			.mobile_hide {{
 				min-height: 0;
 				max-height: 0;
 				max-width: 0;
 				overflow: hidden;
 				font-size: 0px;
-			}
+			}}
 
 			.desktop_hide,
-			.desktop_hide table {
+			.desktop_hide table {{
 				display: table !important;
 				max-height: none !important;
-			}
-		}
+			}}
+		}}
 	</style>
 </head>
 
